1a82a81fa62f0249a07626449ffba8a1392aec91
Mohamed Shemy <msh.work@outlook.sa>
2021-03-14 15:08:26 +0200

    Initial Commit

---
+++ AzkarWatch WatchKit Extension/AzkarWatchApp.swift
@@ -0,0 +1,19 @@
+//
+//  AzkarWatchApp.swift
+//  AzkarWatch WatchKit Extension
+//
+//  Created by Mohamed Shemy on Sun 14 Mar 2021.
+//
+
+import SwiftUI
+
+@main
+struct AzkarWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
